cf61dc3603ef336bd68c8bfdc18c8931060a11d8
saziye <szy.dag2222@gmail.com>
2024-01-03 20:47:04 +0300

    Feature: List added.

---
+++ Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿using System;
+
+namespace threadForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button1.Location = new System.Drawing.Point(509, 457);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(198, 77);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Threadları Çalıştır";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.richTextBox1.Location = new System.Drawing.Point(164, 23);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1000, 130);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.richTextBox2.Location = new System.Drawing.Point(164, 167);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(1000, 130);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.richTextBox3.Location = new System.Drawing.Point(164, 315);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(1000, 130);
+            this.richTextBox3.TabIndex = 3;
+            this.richTextBox3.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.textBox1.Location = new System.Drawing.Point(12, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(146, 36);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Çift Sayılar";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.IndianRed;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.textBox2.Location = new System.Drawing.Point(12, 213);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(147, 36);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "Tek Sayılar";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.LemonChiffon;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.textBox3.Location = new System.Drawing.Point(13, 358);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(146, 36);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "Asal Sayılar";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1185, 546);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace threadForms
+{
+    public partial class Form1 : Form
+    {
+        private static object lockObject = new object();
+        public static ArrayList generalList = new ArrayList();
+        public static ArrayList list1 = new ArrayList();
+        public static ArrayList list2 = new ArrayList();
+        public static ArrayList list3 = new ArrayList();
+        public static ArrayList list4 = new ArrayList();
+        public static ArrayList evenNumberList = new ArrayList();
+        public static ArrayList oddNumberList = new ArrayList();
+        public static ArrayList primeNumberList = new ArrayList();
+        public static int generalListNumberPiece = 100000;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= generalListNumberPiece; i++)
+            {
+                generalList.Add(i);
+            }
+
+            for (int i = 1; i <= generalListNumberPiece / 4; i++)
+            {
+                list1.Add(i);
+            }
+
+            for (int i = generalListNumberPiece / 4 + 1; i <= generalListNumberPiece / 2; i++)
+            {
+                list2.Add(i);
+            }
+
+            for (int i = (generalListNumberPiece / 2) + 1; i <= generalListNumberPiece * 3 / 4; i++)
+            {
+                list3.Add(i);
+            }
+
+            for (int i = (generalListNumberPiece *3 / 4) + 1; i <= generalListNumberPiece; i++)
+            {
+                list4.Add(i);
+            }
+
+            Thread thread1 = new Thread(() => ThreadRun(list1));
+            Thread thread2 = new Thread(() => ThreadRun(list2));
+            Thread thread3 = new Thread(() => ThreadRun(list3));
+            Thread thread4 = new Thread(() => ThreadRun(list4));
+
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+            thread4.Start();
+
+            thread1.Join();
+            thread2.Join();
+            thread3.Join();
+            thread4.Join();
+
+
+            richTextBox1.Text = ListToString(evenNumberList);
+            richTextBox2.Text = ListToString(oddNumberList);
+            richTextBox3.Text = ListToString(primeNumberList);
+
+        }
+
+        private void ThreadRun(ArrayList list)
+        {
+            foreach (int i in list)
+            {
+                lock (lockObject)
+                {
+                    if (EvenNumberCheck(i))
+                    {
+                        evenNumberList.Add(i);
+                    }
+                    if (OddNumberCheck(i))
+                    {
+                        oddNumberList.Add(i);
+                    }
+                    if (PrimeNumberCheck(i))
+                    {
+                        primeNumberList.Add(i);
+                    }
+                }
+            }
+        }
+
+        private bool EvenNumberCheck(int number)
+        {
+            bool result = false;
+            if(number %2 == 0)
+            {
+                result = true;
+            }
+            return result;
+        }
+
+        private bool OddNumberCheck(int number)
+        {
+            bool result = false;
+            if (number % 2 == 1)
+            {
+                result = true;
+            }
+            return result;
+        }
+
+        private bool PrimeNumberCheck(int number)
+        {
+            if (number <= 1)
+            {
+                return false;
+            }
+
+            for (int i = 2; i <= number / 2; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private string ListToString(ArrayList list)
+        {
+            string result = "";
+            foreach (int i in list)
+            {
+                result += i + "  ";
+            }
+            return result;
+        }
+    }
+}
